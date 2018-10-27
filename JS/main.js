d2b52361a9c6bb3f7e6f348b31d8dad574245604
Kara Bayse <karabayse@gmail.com>
2018-10-27 12:19:17 -0500

    added questions array

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+// Questions Array
+const questions = [
+  { question: 'Enter Your First Name' },
+  { question: 'Enter Your Last Name' },
+  { question: 'Enter Your Email', pattern: /\S+@\S+\.\S+/ },
+  { question: 'Create A Password', type: 'password' }
+];
